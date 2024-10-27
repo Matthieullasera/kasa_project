9a908a13b69c591308d402bc6ce32f536f2cd554
Matthieu_llasera <matthieullasera@gmail.com>
2024-10-27 14:12:26 +0100

    upgrade of css with scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import '../src/css/main.css'; 
+import '../src/css/main.scss'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
